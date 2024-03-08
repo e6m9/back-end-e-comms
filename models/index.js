6962f271f4faa3b2d545ff88334383cee39678eb
e6m9 <alostsound@gmail.com>
2024-03-08 17:54:53 -0500

    added product routes

--- models/index.js
+++ models/index.js
@@ -7,29 +7,29 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
+    foreignKey: 'product_id',
+    as: 'tags'
   },
-  as: 'tagged_products'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
+    foreignKey: 'tag_id',
+    as: 'products',
   },
-  as: 'product_tags',
 });
 
 module.exports = {
